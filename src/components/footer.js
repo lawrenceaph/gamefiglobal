9eb9f22f19e7b6b473fcd0a4f757950890c1cacf
Lawrence Aritao <76676277+lawrenceaph@users.noreply.github.com>
2021-10-16 07:40:13 +0800

    updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,5 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { Link } from "gatsby"
-import { RiHeart2Line } from "react-icons/ri"
 
 const Footer = () => (
   <footer
@@ -12,11 +10,8 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
-        <span className="icon -love">
-          <RiHeart2Line />
-        </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        Copyright 2021 GameFi Global
+        
       </p>
     </div>
   </footer>
